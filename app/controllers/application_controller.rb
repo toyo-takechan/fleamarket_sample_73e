5136e366c31b2cd04052b6d4704d0de6d74180cf
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2020-06-21 18:39:11 +0900

    basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials[:basic_auth][:user] &&
+      password == Rails.application.credentials[:basic_auth][:pass]
+    end
+  end
+
+  before_action :basic_auth, if: :production?
+
+private
+# 以下を追記
+def production?
+  Rails.env.production?
+end
 end
