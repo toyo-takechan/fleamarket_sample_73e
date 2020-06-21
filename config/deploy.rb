d74d74a27dfdba47648d2e5326e02eb3fa739a7e
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2020-06-21 18:18:14 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.14.1'
 
 # 自身のアプリ名、リポジトリ名を記述
 set :application, 'fleamarket_sample_73e'
-set :repo_url,  'git@github.com:toyo-takchan/fleamarket_sample_73e.git'
+set :repo_url,  'git@github.com:toyo-takechan/fleamarket_sample_73e.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
