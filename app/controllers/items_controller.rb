b9cdfad1ad009e85016c0deaa4d09b92f1a922a4
atsukofu <atsukofu0527@gmail.com>
2020-07-12 00:56:22 +0900

    add toppage itemdetail

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.first(4)
+    @brandItems = Item.where(brand: "ABC")
   end
 
   def confirm
@@ -7,6 +9,7 @@ class ItemsController < ApplicationController
   
   def show
     @item = Item.find(params[:id])
+    @relatedItems = Item.where(category_id: @item.category_id)
   end
 
   
