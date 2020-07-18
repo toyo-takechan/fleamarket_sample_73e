f88136a6132b61ab8ee9e6c6376ec8edcf8bd4a1
jungle379 <jungle379@gmail.com>
2020-07-18 13:18:10 +0900

    add-sinki

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show,:edit]
+
   def index
     @user = User.new
     @profile = Profile.new
@@ -9,6 +11,10 @@ class UsersController < ApplicationController
   end
   
   def edit
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def update
@@ -25,7 +31,11 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:nickname, :email).merge(user_id: current_user.id)
+    params.require(:user).permit(:nickname, :email)
   end
   
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
