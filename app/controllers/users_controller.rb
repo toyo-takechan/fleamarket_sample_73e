de6421144db320fadca3d9ea4899d2094264de55
jungle379 <jungle379@gmail.com>
2020-07-18 14:05:33 +0900

    syuusei

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,6 @@ class UsersController < ApplicationController
   def edit
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
-
   def update
     if current_user.update(user_params)
       redirect_to root_path
