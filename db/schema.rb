2472562b9009658e0bd7d5e4bd8b275fd00e4035
kouhei <kobuncruisin185@yahoo.co.jp>
2020-07-29 23:16:20 +0900

    redo-add-edit/side/card/logout-btn-link

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_120948) do
+ActiveRecord::Schema.define(version: 2020_07_11_051104) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name", null: false
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2020_07_22_120948) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -85,9 +86,7 @@ ActiveRecord::Schema.define(version: 2020_07_22_120948) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "nickname", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true
   end
 
 end
