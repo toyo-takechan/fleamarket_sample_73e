f1dc15fe2b841fefd7539402c3fc52126ca190dd
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2020-08-10 19:42:03 +0900

    rails db:migrate:resetコマンドを実施

--- db/schema.rb
+++ db/schema.rb
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2020_08_06_125900) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -87,6 +88,7 @@ ActiveRecord::Schema.define(version: 2020_08_06_125900) do
     t.datetime "updated_at", null: false
     t.string "nickname", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
