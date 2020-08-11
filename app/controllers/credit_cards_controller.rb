b6364d440e4fb46b400d18a1c1ee61997c9d62fc
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2020-08-12 07:24:06 +0900

    Creditを追記

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -6,23 +6,23 @@ class CreditCardsController < ApplicationController
   require "payjp"
 
   def new
-    redirect_to card_path(current_user.id) if @card.present?
+    redirect_to credit_card_path(current_user.id) if @card.present?
   end
 
   def create
     Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_SECRET_KEY]
     if params['payjp-token'].blank?
-      redirect_to new_card_path
+      redirect_to new_credit_card_path
     else
       customer = Payjp::Customer.create(
       card: params['payjp-token'],
       metadata: {user_id: current_user.id}
       )
-      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
-        redirect_to registration_done_card_index_path
+        redirect_to registration_done_credit_card_index_path
       else
-        redirect_to new_card_path
+        redirect_to new_credit_card_path
       end
     end
   end
@@ -32,7 +32,7 @@ class CreditCardsController < ApplicationController
 
   def show
     if @card.blank?
-      redirect_to new_card_path 
+      redirect_to new_credit_card_path 
     else
       Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_SECRET_KEY]
       customer = Payjp::Customer.retrieve(@card.customer_id)
@@ -62,7 +62,7 @@ class CreditCardsController < ApplicationController
       customer.delete
       @card.delete
     end
-      redirect_to delete_done_card_index_path
+      redirect_to delete_done_credit_card_index_path
   end
 
   def delete_done
@@ -98,7 +98,7 @@ class CreditCardsController < ApplicationController
 
   def pay
     if @item.auction_status == "売り切れ"
-      redirect_to buy_card_path(@item)
+      redirect_to buy_credit_card_path(@item)
     else
       if current_user.card.present?
         Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_SECRET_KEY]
@@ -125,7 +125,7 @@ class CreditCardsController < ApplicationController
   end
 
   def set_card
-    @card = Card.find_by(user_id: current_user.id)
+    @card = CreditCard.find_by(user_id: current_user.id)
   end
 
   def set_item
