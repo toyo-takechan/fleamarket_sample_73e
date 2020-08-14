c9f8edab4f31f217ac76cdb79992c6ebd8b343b5
jungle379 <jungle379@gmail.com>
2020-08-14 21:47:33 +0900

    syuusei-2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_14_122749) do
+ActiveRecord::Schema.define(version: 2020_08_14_124534) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "post_code", null: false
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 2020_08_14_122749) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ancestry"
-    t.string "index"
     t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
