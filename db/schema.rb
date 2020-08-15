45d125de629258d28f3c010060bd6615a741c7e6
中西泰朗 <yasuo.nakanishi.0930@gmail.com>
2020-08-15 12:28:38 +0900

    seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_045705) do
+ActiveRecord::Schema.define(version: 2020_08_14_151839) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "post_code", null: false
@@ -77,7 +77,6 @@ ActiveRecord::Schema.define(version: 2020_08_13_045705) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -87,7 +86,6 @@ ActiveRecord::Schema.define(version: 2020_08_13_045705) do
     t.datetime "updated_at", null: false
     t.string "nickname", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
